f86a13f142ceefac6153b971225fcb5f3223dd06
Jay Cortes <cortesjay1991@gmail.com>
2024-09-21 13:05:16 +0200

    API is working and has been tested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,16 @@ class PostsController < ApplicationController
 
   def create
     # user.post.create(post_params)
-    if post = @user.post.create(post_params)
+    if post = @user.posts.create(post_params)
     render json: post.as_json
     else
       render json: { message: "Issue creating post", errors: post.errors.full_messages }
     end
+  end
+
+    private
 
     def post_params
       params.require(:post).permit(:caption, :image_url, :body)
     end
-  end
 end
